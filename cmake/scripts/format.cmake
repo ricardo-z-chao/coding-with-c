7eaf430c5288adcca2961128afa382f821627c11
ricardo <1968808192@qq.com>
2025-10-28 12:36:24 +0800

    update format script

--- cmake/scripts/format.cmake
+++ cmake/scripts/format.cmake
@@ -2,21 +2,34 @@ function(format_source)
   find_program(CLANG_FORMAT_PATH clang-format REQUIRED)
   set(PATTERNS)
 
-  foreach(item ${ARGN})
-    set(EXTENSION h c cc cpp)
-    list(TRANSFORM EXTENSION PREPEND "${item}/*.")
-    list(APPEND PATTERNS ${EXTENSION})
-  endforeach()
-
   file(
     GLOB_RECURSE SOURCE_FILES FOLLOW_SYMLINKS
     LIST_DIRECTORIES false
-    ${PATTERNS})
+    *.c *.h *.cc *.cpp)
+
+  list(FILTER SOURCE_FILES EXCLUDE REGEX ".*/out/.*")
 
   add_custom_target(
     format-source ALL
     COMMAND ${CLANG_FORMAT_PATH} -i --verbose --style=LLVM ${SOURCE_FILES}
     COMMAND_EXPAND_LISTS
     COMMENT "formatting all source code"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+endfunction()
+
+function(format_cmake_file)
+  find_program(CMAKE_FORMAT_PATH cmake-format REQUIRED)
+
+  file(
+    GLOB_RECURSE CMAKE_FILES FOLLOW_SYMLINKS
+    LIST_DIRECTORIES false
+    CMakeLists.txt ${CMAKE_SOURCE_DIR}/*.cmake)
+
+  list(FILTER CMAKE_FILES EXCLUDE REGEX ".*/out/.*")
+
+  add_custom_target(
+    format-cmake-list ALL
+    COMMAND ${CMAKE_FORMAT_PATH} -i ${CMAKE_FILES}
+    COMMENT "formatting all CMake files"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 endfunction()
