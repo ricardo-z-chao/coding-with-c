a3441040d5474688feb5eb3f9bc6bc532913b111
ricardo <1968808192@qq.com>
2025-11-19 01:47:47 +0800

    chore(lint): add lint support

--- cmake/scripts/format_lint.cmake
+++ cmake/scripts/format_lint.cmake
@@ -1,21 +1,32 @@
 include_guard()
 
+# find all source files
+file(
+  GLOB_RECURSE SOURCE_FILES FOLLOW_SYMLINKS
+  LIST_DIRECTORIES false
+  *.c *.h *.cc *.cpp)
+list(FILTER SOURCE_FILES EXCLUDE REGEX ".*/out/.*")
+
 function(format_source)
   find_program(CLANG_FORMAT_PATH clang-format REQUIRED)
   set(PATTERNS)
-
-  file(
-    GLOB_RECURSE SOURCE_FILES FOLLOW_SYMLINKS
-    LIST_DIRECTORIES false
-    *.c *.h *.cc *.cpp)
-
-  list(FILTER SOURCE_FILES EXCLUDE REGEX ".*/out/.*")
 
   add_custom_target(
     format-source ALL
     COMMAND ${CLANG_FORMAT_PATH} -i --verbose --style=LLVM ${SOURCE_FILES}
     COMMAND_EXPAND_LISTS
     COMMENT "formatting all source code"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+endfunction()
+
+function(lint)
+  find_program(CPP_LINT cpplint REQUIRED)
+
+  add_custom_target(
+    cpplint ALL
+    COMMAND ${CPP_LINT} ${SOURCE_FILES}
+    COMMAND_EXPAND_LISTS
+    COMMENT "running cpplint"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 endfunction()
 
