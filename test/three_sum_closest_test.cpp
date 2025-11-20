bf6fdd10350466182397fba98879808b0fd312f2
ricardo <1968808192@qq.com>
2025-11-21 00:35:04 +0800

    test: add leetcode-16 test case

---
+++ test/three_sum_closest_test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <vector>
+extern "C" {
+#include "three_sum_closest.h"
+}
+
+struct TestData {
+  std::vector<int> nums;
+  int target;
+  int result;
+};
+
+class ThreeSumClosestTest : public ::testing::TestWithParam<TestData> {};
+
+TEST_P(ThreeSumClosestTest, ThreeSumClosest) {
+  TestData testData = GetParam();
+  int *nums = testData.nums.data();
+  int target = testData.target;
+  int result = testData.result;
+  int closest = threeSumClosest(nums, testData.nums.size(), target);
+  EXPECT_EQ(closest, result);
+}
+
+INSTANTIATE_TEST_SUITE_P(LeetCodeTest, ThreeSumClosestTest,
+                         ::testing::Values(TestData{{-1, 2, 1, -4}, 1, 2},
+                                           TestData{{0, 0, 0}, 1, 0}));
